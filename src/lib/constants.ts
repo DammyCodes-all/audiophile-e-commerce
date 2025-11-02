94abffa97dbbd990b7be90c11de24552b3ecc62e
DammyCodes-all <harliarmeen@gmail.com>
2025-11-02 03:11:45 +0100

    fix: Update headphone hrefs for consistency and accuracy

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -27,19 +27,19 @@ export const productShowcaseData = [
 export const headphoneProductList = [
   {
     name: "xx99 mark II",
-    href: "/headphones/xx99-mark-II-headphone",
+    href: "/headphones/xx99-mark-two-headphones",
     imageUrl: "/images/headphones/xx99-markII-headphone.jpg",
     desc: "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
   },
   {
     name: "xx99 mark I",
-    href: "/headphones/xx99-mark-I-headphone",
+    href: "/headphones/xx99-mark-one-headphones",
     imageUrl: "/images/headphones/xx99-markI-headphone.jpg",
     desc: "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
   },
   {
     name: "xx59",
-    href: "/headphones/xx59-headphone",
+    href: "/headphones/xx59-headphones",
     imageUrl: "/images/headphones/xx59-headphone.jpg",
     desc: "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.",
   },
