40be94a6a5a01e070f03a978a62102a11da107a0
Olagunju Alameen Olasubomi <harliarmeen@gmail.com>
2025-11-06 14:05:01 +0100

    Update page-head.tsx

--- src/components/page-head.tsx
+++ src/components/page-head.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageHead = ({ children }: { children: React.ReactNode }) => {
   return (
-    <header className="font-bold tracing-[1.43px] flex items-center justify-center text-4xl h-[239px] w-full bg-theme-dark uppercase">
+    <header className="font-bold tracing-[1.43px] flex items-center justify-center text-4xl h-[150px] md:h-[239px] w-full bg-theme-dark uppercase">
       {children}
     </header>
   );
